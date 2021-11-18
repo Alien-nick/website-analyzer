a466b3553f0693b356aa68f8fd6e114311df79ec
Nicholas Seetaram <nicholas.daniel.seetaram@gmail.com>
2021-11-18 11:29:28 -0400

    Update analyzer.sh

--- analyzer.sh
+++ analyzer.sh
@@ -2,5 +2,5 @@
 sed 1d sites.csv | while IFS="," read -r name domain
 do
    curl -s -w 'Testing Website Response Time for :%{url_effective}\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}
-\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null $domain
+\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null $domain >> result.csv
 done
